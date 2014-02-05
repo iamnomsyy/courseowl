d9a819a4d99a151d393f1b1326dc174b405f0421
Mike Staszel <crimsonredmk@gmail.com>
2014-02-04 20:19:33 -0800

    settings and postgres

--- courseowl_django/courseowl_django/settings.py
+++ courseowl_django/courseowl_django/settings.py
@@ -55,8 +55,11 @@ WSGI_APPLICATION = 'courseowl_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'owl',
+        'USER': 'owl',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'localhost'
     }
 }
 

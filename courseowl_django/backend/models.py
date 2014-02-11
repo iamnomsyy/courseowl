34659db65834ac61fc244d39401a531096822084
David Eisenberg <eisenbe7@illinois.edu>
2014-02-10 20:46:30 -0600

    Added model skeletons.

--- courseowl_django/backend/models.py
+++ courseowl_django/backend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    subjects = models.ManyToManyField(Subject)
+    provider = models.ForeignKey(Provider)
+    description = models.CharField(max_length=3000)
+    similarCourses = models.ManyToManyField('self')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+class Provider(models.Model):
+    name = models.CharField(max_length=100)

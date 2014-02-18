94c40838633134d32904b23685ed019d86d65d9b
David Eisenberg <eisenbe7@illinois.edu>
2014-02-17 20:24:17 -0600

    Added coursera import script.

--- courseowl_django/courses/models.py
+++ courseowl_django/courses/models.py
@@ -7,6 +7,7 @@ class Course(models.Model):
     description = models.CharField(max_length=3000)
     similarCourses = models.ManyToManyField('self')
     instructor = models.CharField(max_length=1000)
+    source = models.ForeignKey(Source)
 
     def __unicode__(self):
         return self.name
@@ -21,5 +22,11 @@ class Subject(models.Model):
 class Provider(models.Model):
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 class Source(models.Model):
     name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
---
+++ courseowl_django/courses/scripts/coursera.py
@@ -0,0 +1,25 @@
+from ..models import *
+import json
+import urllib2
+
+
+
+def run():
+    courseJsonList = urllib2.urlopen('https://www.coursera.org/maestro/api/topic/list?full=1%20or%20https://www.coursera.org/maestro/api/topic/list2')
+    courseraJson = json.loads(courseJsonList.read())
+    courseraProvider, created = Provider.objects.get_or_create(name='coursera')
+
+    for course in courseraJson:
+        c, created = Course.objects.get_or_create(name=course['name'], description=course['short_description'], instructor=course['instructor'])
+        c.provder = courseraProvider
+        c.source, created = Source.objects.get_or_create(name=course['universities'][0]['name'])
+        c.save()
+        for categoryId in course['category-ids']:
+            subject, created = Subject.objects.get_or_create(name=categoryId)
+            c.subjects.add(subject)
+        c.save()
+
+
+
+if __name__ == '__main__':
+    run()

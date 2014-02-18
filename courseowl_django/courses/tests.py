3cce66042a490551c16419e96d62593b04937cd6
Riff Schelder <riff@schelder.com>
2014-02-17 21:21:46 -0600

    add a test for Subject

--- courseowl_django/courses/tests.py
+++ courseowl_django/courses/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from courses.models import Subject, Provider, Source, Course
 
-# Create your tests here.
+class SubjectTests(TestCase):
+    def test_create_subject(self):
+        subject = Subject()
+        subject.name = 'CS'
+        subject.save()
+
+        all_subjects = Subject.objects.all()
+        self.assertEquals(len(all_subjects), 1)
+        only_subject = all_subjects[0]
+        self.assertEquals(only_subject, subject)
+
+        self.assertEquals(only_subject.name, 'CS')

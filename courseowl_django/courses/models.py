4d320d824b08f6e6199a7a6cded08cbe6d63e634
David Eisenberg <eisenbe7@illinois.edu>
2014-02-17 20:24:31 -0600

    Added coursera import script.

--- courseowl_django/courses/models.py
+++ courseowl_django/courses/models.py
@@ -1,32 +1,31 @@
 from django.db import models
 
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    subjects = models.ManyToManyField(Subject)
-    provider = models.ForeignKey(Provider)
-    description = models.CharField(max_length=3000)
-    similarCourses = models.ManyToManyField('self')
-    instructor = models.CharField(max_length=1000)
-    source = models.ForeignKey(Source)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Subject(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
 
     def __unicode__(self):
         return self.name
 
 class Provider(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
 
     def __unicode__(self):
         return self.name
 
 class Source(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=1000)
+    subjects = models.ManyToManyField(Subject)
+    provider = models.ForeignKey(Provider, null=True, blank=True)
+    description = models.CharField(max_length=3000)
+    similarCourses = models.ManyToManyField('self')
+    instructor = models.CharField(max_length=1000, null=True, blank=True)
+    source = models.ForeignKey(Source, null=True, blank=True)
 
     def __unicode__(self):
         return self.name

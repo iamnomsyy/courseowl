1c85ee4f0c4be0f84991fc75a2ad256be6a29386
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-02-20 21:20:49 -0600

    allauth settings

--- courseowl_django/accounts/urls.py
+++ courseowl_django/accounts/urls.py
@@ -3,5 +3,4 @@ import views
 
 urlpatterns = patterns('',
                        url(r'^login', views.login, name='login'),
-                       # url(r'^google_login', views.google_login, name='google login')
                        )
--- courseowl_django/courseowl_django/urls.py
+++ courseowl_django/courseowl_django/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^accounts/', include('accounts.urls')),
+                       url(r'^social_accounts/', include('allauth.urls')),
                        url(r'^$', include('website.urls')),
                        )

a421c1edcde472b55ff517a6fbbde7faab5344ea
Riff Schelder <riff@schelder.com>
2014-02-20 19:35:38 -0600

    register 4 course models to admin site

--- courseowl_django/courses/admin.py
+++ courseowl_django/courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subject, Provider, Source, Course
 
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(Provider)
+admin.site.register(Source)
+admin.site.register(Course)

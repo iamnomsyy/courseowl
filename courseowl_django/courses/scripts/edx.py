dbf5ec63b9c3702053ef3ef930e015f6d8248762
David Eisenberg <eisenbe7@illinois.edu>
2014-02-27 21:15:06 -0600

    Complete edx course importing.

--- courseowl_django/courses/scripts/edx.py
+++ courseowl_django/courses/scripts/edx.py
@@ -1,9 +1,10 @@
 __author__ = 'David'
 
-#from ..models import *
+from ..models import *
 import urllib2
 from bs4 import BeautifulSoup
 import re
+
 
 subjects = [
     'business-management',
@@ -31,47 +32,63 @@ subjects = [
     'social-sciences',
     'biology-life-sciences',
     'statistics-data-analysis'
-]
+    ]
+
+intList = list()
+uniList = list()
+titleList = list()
+descList = list()
+subList = list()
 
 
 def run():
+    populateLists()
+    addToDjango()
+
+
+def populateLists():
+    print "Adding courses from edx (this will take a minute)..."
     for subject in subjects:
-        for i in range(10):
-            titleList = list()
-            descList = list()
+        for i in range(5):
             urlStr = 'https://www.edx.org/course-list/allschools/' + subject + '/allcourses'
             if i >= 1:
                 urlStr = urlStr + '?page=' + unicode(i)
-            print urlStr
             edxWeb = BeautifulSoup(urllib2.urlopen(urlStr).read())
             for child in edxWeb.find_all('h2', attrs={'class' : 'title course-title'}):
                 courseInfo = child.parent
                 try:
-                    print courseInfo.a.contents[0]
-                    titleList.append(courseInfo.a.contents[0])
-                    print courseInfo.div.contents[0]
-                    descList.append(courseInfo.div.contents[0])
-                    print '\n\n'
+                    titleList.append(courseInfo.a.contents[0].rstrip('\n'))
+                    descList.append(courseInfo.div.contents[0].rstrip('\n'))
+                    subList.append(subject)
                 except:
                     pass
 
-            intList = list()
-            uniList = list()
             for instInfo in edxWeb.find_all('ul', attrs={'class' : 'clearfix'}):
                 instructor = re.search('(?<=Instructors:</span>)(.*)(?=</li>)', unicode(instInfo))
                 uni = re.search('(?<=<li><strong>)(.*)(?=</strong></li>)', unicode(instInfo))
                 try:
-                    print instructor.group(0)
-                    print uni.group(0)
-                    intList.append(instructor.group(0))
-                    uniList.append(uni.group(0))
+                    intList.append(instructor.group(0).rstrip('\n'))
+                    uniList.append(uni.group(0).rstrip('\n'))
                 except:
                     pass
+
+
+def addToDjango():
+    edxProvider, created = Provider.objects.get_or_create(name='edx')
     for i in range(len(titleList)):
-        print 'Title: ' + titleList[i]
-        print 'Description: ' + descList[i]
-        print 'Instructor: ' + intList[i]
-        print 'University: ' + uniList[i] + '\n\n'
+        try:
+            print "Adding " + titleList[i] + " course: " + str(i)
+        except:
+            pass
+        c, created = Course.objects.get_or_create(name=titleList[i], description=descList[i], instructor=intList[i])
+        c.provider = edxProvider
+        c.source, created = Source.objects.get_or_create(name=uniList[i])
+        c.save()
+        subject, created = Subject.objects.get_or_create(name=subList[i])
+        c.subjects.add(subject)
+        c.save()
+    print "Done!"
+    print str(len(titleList)) + " courses added!"
 
 
 

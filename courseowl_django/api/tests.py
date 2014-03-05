9f3e34f3a690e6eff3a8d4e096a40c41b4a0cb88
Mike Staszel <crimsonredmk@gmail.com>
2014-03-04 21:25:56 -0600

    some tests for api

--- courseowl_django/api/tests.py
+++ courseowl_django/api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test.client import Client
+from courses.models import Subject, Course
 
-# Create your tests here.
+
+class TestAPI(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_json_subjects(self):
+        temp_subject = Subject(name='Pottery')
+        temp_subject.save()
+        response = self.client.get('/api/subjects')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Pottery' in response.content)
+
+    def test_json_courses(self):
+        temp_subject = Course(name='Advanced Pottery III')
+        temp_subject.save()
+        response = self.client.get('/api/courses')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Advanced Pottery III' in response.content)
--- courseowl_django/api/views.py
+++ courseowl_django/api/views.py
@@ -6,43 +6,52 @@ from courses.models import Subject, Course
 
 
 def json_subjects(request):
+    """
+    Return a JSON array of all the subjects in the CourseOwl database.
+    Method: GET
+    """
     subject_arr = []
     for subject in Subject.objects.all():
         subject_arr.append(subject.name)
-    return HttpResponse(json.dumps(subject_arr), mimetype='application/json')
+    return HttpResponse(json.dumps(subject_arr), content_type='application/json')
 
 
 def json_courses(request):
+    """
+    Return a JSON array of all the courses in the CourseOwl database.
+    Method: GET
+    """
     course_arr = []
     for course in Course.objects.all():
         course_arr.append(course.name)
-    return HttpResponse(json.dumps(course_arr), mimetype='application/json')
+    return HttpResponse(json.dumps(course_arr), content_type='application/json')
 
 
 @login_required
 def json_enrolled_courses(request):
     """
-    Return a JSON array of courses that request.user is signed up for (make a POST request).
+    Return a JSON array of courses that request.user is signed up for.
+    Method: GET
     """
     enrolled_arr = []
-    if request.method == 'POST':
-        user_profile = UserProfile.objects.get(user=request.user)
+    user_profile = UserProfile.objects.get(user=request.user)
 
-        for course in user_profile.enrolled.objects.all():
-            enrolled_arr.append(course.name)
-    return HttpResponse(json.dumps(enrolled_arr), mimetype='application/json')
+    for course in user_profile.enrolled.objects.all():
+        enrolled_arr.append(course.name)
+    return HttpResponse(json.dumps(enrolled_arr), content_type='application/json')
+
 
 @login_required
 def json_liked_subjects(request):
     """
-    Return a JSON array of courses that request.user has liked (make a POST request).
+    Return a JSON array of courses that request.user has liked.
+    Method: GET
     """
     liked_arr = []
-    if request.method == 'POST':
-        user_profile = UserProfile.objects.get(user=request.user)
+    user_profile = UserProfile.objects.get(user=request.user)
 
-        for subject in user_profile.interests.objects.all():
-            liked_arr.append(subject.name)
+    for subject in user_profile.interests.objects.all():
+        liked_arr.append(subject.name)
     return HttpResponse(json.dumps(liked_arr), content_type='application/json')
 
 
@@ -50,6 +59,7 @@ def json_liked_subjects(request):
 def like_subject(request):
     """
     POST here when you like a subject with a 'liked_subject' data property.
+    Method: POST, {'liked_subject': 'name of subject'}
     """
     if request.method == 'POST':
         success = True

ace564e33144cd378be740438f344c65b249201f
Mike Staszel <crimsonredmk@gmail.com>
2014-03-04 20:39:47 -0600

    made api endpoints for liking a subject and getting what subjects users like

--- courseowl_django/api/urls.py
+++ courseowl_django/api/urls.py
@@ -4,5 +4,7 @@ import views
 urlpatterns = patterns('',
                        url(r'^subjects', views.json_subjects, name='api_subjects'),
                        url(r'^courses', views.json_courses, name='api_courses'),
-                       url(r'^enrolled_courses', views.json_enrolled_courses, name='enrolled_courses')
+                       url(r'^enrolled_courses', views.json_enrolled_courses, name='enrolled_courses'),
+                       url(r'^liked_subjects', views.json_liked_subjects, name='liked_subjects'),
+                       url(r'^like_subject', views.like_subject, name='like_subject')
                        )
--- courseowl_django/api/views.py
+++ courseowl_django/api/views.py
@@ -1,7 +1,6 @@
 import json
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from accounts.models import UserProfile
 from courses.models import Subject, Course
 
@@ -32,3 +31,34 @@ def json_enrolled_courses(request):
         for course in user_profile.enrolled.objects.all():
             enrolled_arr.append(course.name)
     return HttpResponse(json.dumps(enrolled_arr), mimetype='application/json')
+
+@login_required
+def json_liked_subjects(request):
+    """
+    Return a JSON array of courses that request.user has liked (make a POST request).
+    """
+    liked_arr = []
+    if request.method == 'POST':
+        user_profile = UserProfile.objects.get(user=request.user)
+
+        for subject in user_profile.interests.objects.all():
+            liked_arr.append(subject.name)
+    return HttpResponse(json.dumps(liked_arr), mimetype='application/json')
+
+
+@login_required
+def like_subject(request):
+    """
+    POST here when you like a subject with a 'liked_subject' data property.
+    """
+    if request.method == 'POST':
+        success = True
+        subject_name = request['liked_subject']
+        user_profile = UserProfile.objects.get(user=request.user)
+        try:
+            user_profile.interests.objects.add(Subject.objects.get(name=subject_name))
+        except:
+            success = False
+        return HttpResponse(json.dumps({'success': success}))
+    else:
+        return HttpResponse(json.dumps({'success': False}))

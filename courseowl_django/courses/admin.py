6a6c086a19e9b2a78e074d6ecf3b6210ff4b68ee
Riff Schelder <riff@schelder.com>
2014-03-05 21:17:30 -0600

    add a search bar to the Course model in admin panel, because there are too many courses

--- courseowl_django/courses/admin.py
+++ courseowl_django/courses/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Subject, Provider, Source, Course
 
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+     search_fields = ['name']
+
 admin.site.register(Subject)
 admin.site.register(Provider)
 admin.site.register(Source)
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)

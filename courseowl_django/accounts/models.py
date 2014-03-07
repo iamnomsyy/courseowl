d37ade0a30db3fe1f4ab240316bc2120c1a6d2ed
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-03-06 23:05:11 -0600

    Needed to make it work for some reason

--- courseowl_django/accounts/models.py
+++ courseowl_django/accounts/models.py
@@ -6,6 +6,6 @@ from courses.models import *
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     gender = models.CharField(max_length=1, default='M')
-    interests = models.ManyToManyField('courses.Subject')
-    providers = models.ManyToManyField('courses.Provider')
-    enrolled = models.ManyToManyField('courses.Course')
+    interests = models.ManyToManyField('courses.Subject', blank=True)
+    providers = models.ManyToManyField('courses.Provider', blank=True)
+    enrolled = models.ManyToManyField('courses.Course', blank=True)

db1620fda401239d5b913ffd1a9971bd0a9c1aba
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-03-06 23:08:24 -0600

    changed urls and added POST/GET work

--- courseowl_django/website/views.py
+++ courseowl_django/website/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from courses.models import Subject, Course
+from accounts.models import UserProfile
+import json
 
 
 def index(request):
@@ -7,6 +10,33 @@ def index(request):
 
 
 @login_required
-def personalize(request):
-    context = {}
-    return render(request, 'website/personalize.html', context)
+def subject_preferences(request):
+	if request.method == 'POST':
+		subject_ids = json.loads(request.POST.get('subject_ids'))
+		# Save subjects to user's profile
+		user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+		if created:
+			user_profile.save()
+		for sub_id in subject_ids:
+			subject = Subject.objects.get(id=sub_id)
+			interests = user_profile.interests.add(subject)
+		user_profile.save()
+		return redirect('/course_preferences')
+
+	context = {'subjects': Subject.objects.all().extra(order_by=['name'])}
+	return render(request, 'website/personalize_subjects.html', context)
+
+
+@login_required
+def course_preferences(request):
+	if request.method == 'POST':
+		course_ids = json.loads(request.POST.get('course_ids'))
+		user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+		for course in course_ids:
+			user_profile.enrolled.add(Course.objects.get(id=course))
+		user_profile.save()
+		return redirect('/accounts/profile')
+
+	liked_subjects = UserProfile.objects.get(user=request.user).interests.all()
+	context = {'courses': Course.objects.filter(subjects__in=liked_subjects)}
+	return render(request, 'website/personalize_courses.html', context)

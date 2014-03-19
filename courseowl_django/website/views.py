dfb852496854f8e7954759768cb57e4842e585b4
Riff Schelder <riff@schelder.com>
2014-03-18 19:54:51 -0500

    delete old search

--- courseowl_django/website/views.py
+++ courseowl_django/website/views.py
@@ -40,33 +40,3 @@ def course_preferences(request):
     liked_subjects = UserProfile.objects.get(user=request.user).interests.all()
     context = {'courses': Course.objects.filter(subjects__in=liked_subjects)}
     return render(request, 'website/personalize_courses.html', context)
-
-
-def search(request):
-    query = request.GET.get('query')
-    
-    # test data for iteration 2
-    courses = [ {'name': 'Introduction to Programming', 
-                 'prov': 'Coursera',
-                 'instr': 'J. Random Teacher I',
-                 'src': 'Stanford'},
-                 {'name': 'Introduction to Statistics', 
-                 'prov': 'Coursera',
-                 'instr': 'J. Random Teacher II',
-                 'src': 'MIT'},
-                 {'name': 'Introduction to AI', 
-                 'prov': 'Udacity',
-                 'instr': 'J. Random Teacher III',
-                 'src': 'UC Berkeley'},
-                 {'name': 'Introduction to Bagels', 
-                 'prov': 'EdX',
-                 'instr': 'J. Random Teacher I',
-                 'src': 'Google'},
-                 {'name': 'Introduction to Google', 
-                 'prov': 'EdX',
-                 'instr': 'J. Random Teacher I',
-                 'src': 'Microsoft'},
-              ]
-
-    context = {'query': query, 'courses': courses}
-    return render(request, 'website/search.html', context)

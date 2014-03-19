527db208729546c1da3ce77819d7f22f3a6af6d3
David Eisenberg <eisenbe7@illinois.edu>
2014-03-18 19:47:03 -0500

    Add delete account.

--- courseowl_django/accounts/urls.py
+++ courseowl_django/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
                        url(r'^login', views.login, name='login'),
                        url(r'^signup', views.email_signup, name='email_signup'),
                        url(r'^logout', views.logout, name='logout'),
+                       url(r'^delete_account', views.delete_account, name='delete+account'),
                        url(r'^profile', views.profile, name='profile')
                        )

aa67647cbbcfc6bb9f6cf0c768e87f5a50cc6f7c
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-03-20 08:53:44 -0500

    Added some sign up tests

--- courseowl_django/frontend-tests/tests.js
+++ courseowl_django/frontend-tests/tests.js
@@ -3,7 +3,7 @@ var siteUrl = 'http://127.0.0.1:8000/'; // not sure how to deal with production 
 casper.test.begin('Homepage works', function suite(test) {
   casper.start(siteUrl, function() {
      test.assertHttpStatus(200, 'Site is up and running');
-     var signupButtonSelector = 'a[href="/accounts/signup/"]';
+     var signupButtonSelector = '#signup-button';
      test.assertExists(signupButtonSelector, 'Signup button present');
      this.click(signupButtonSelector);
   });
@@ -14,7 +14,7 @@ casper.test.begin('Homepage works', function suite(test) {
   });
 
   casper.thenOpen(siteUrl, function() {
-    var signInSelector = 'a[href="/accounts/login/"]';
+    var signInSelector = '#signin-button';
     test.assertExists(signInSelector, 'Sign in button exists');
     this.click(signInSelector);
   });
@@ -29,5 +29,27 @@ casper.test.begin('Homepage works', function suite(test) {
 
   casper.run(function() {
       test.done();
+  });
+});
+
+casper.test.begin('Sign up process', function suite(test) {
+  casper.start(siteUrl+'accounts/signup', function() {
+    // TODO: find better way of testing new account creation with temp accounts
+    var emailAddress = Date.now().toString() + '@test.com';
+    var signupUrl = this.getCurrentUrl();
+
+    test.assertExists('form', 'Sign up form exists');
+    this.fill('form', {
+      'email': '',
+      'password': '',
+      'password_confirm': ''
+    }, true);
+
+    test.assert(this.getCurrentUrl() === signupUrl, 'Empty submit back to signup');
+
+  });
+
+  casper.run(function() {
+    test.done();
   });
 });

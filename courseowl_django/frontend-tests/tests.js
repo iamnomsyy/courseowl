e2c4df68f9830121ff4abdfebd9af098a4a22bc1
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-03-19 21:21:48 -0500

    added some comprehensive ui tests for the homepage

--- courseowl_django/frontend-tests/tests.js
+++ courseowl_django/frontend-tests/tests.js
@@ -1,9 +1,33 @@
-var should = require('should');
+var siteUrl = 'http://127.0.0.1:8000/'; // not sure how to deal with production vs dev
 
-describe('WebLoading', function() {
-	describe('Homepage', function() {
-		it('should load an html page when the request is made', function() {
+casper.test.begin('Homepage works', function suite(test) {
+  casper.start(siteUrl, function() {
+     test.assertHttpStatus(200, 'Site is up and running');
+     var signupButtonSelector = 'a[href="/accounts/signup/"]';
+     test.assertExists(signupButtonSelector, 'Signup button present');
+     this.click(signupButtonSelector);
+  });
 
-		});
-	});
+  // Clicked signup button, should be on signup page
+  casper.then(function() {
+    test.assert(this.getCurrentUrl() === (siteUrl + 'accounts/signup/'), 'Sign up page accessible from home page');
+  });
+
+  casper.thenOpen(siteUrl, function() {
+    var signInSelector = 'a[href="/accounts/login/"]';
+    test.assertExists(signInSelector, 'Sign in button exists');
+    this.click(signInSelector);
+  });
+
+  // Clicked sign in button
+  casper.then(function() {
+    test.assert(
+      this.getCurrentUrl() === (siteUrl + 'accounts/login/'),
+      'Login accessible from home page'
+    );
+  });
+
+  casper.run(function() {
+      test.done();
+  });
 });

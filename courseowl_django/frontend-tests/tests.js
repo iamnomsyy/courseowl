f076c59c8ce7d3ebc178f554c2d16d95cec94521
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-03-19 20:23:22 -0500

    Tests file

---
+++ courseowl_django/frontend-tests/tests.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+describe('WebLoading', function() {
+	describe('Homepage', function() {
+		it('should load an html page when the request is made', function() {
+
+		});
+	});
+});

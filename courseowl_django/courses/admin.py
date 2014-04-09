de3fa6ff21834dcd4ce7e708763d2cb4e1ff0ab0
Mike Staszel <crimsonredmk@gmail.com>
2014-04-08 19:35:53 -0500

    cleanup courses.admin

--- courseowl_django/courses/admin.py
+++ courseowl_django/courses/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Subject, Provider, Source, Course
+from courses.models import Subject, Provider, Source, Course
 
-# Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
-     search_fields = ['name']
+    search_fields = ['name']
 
 admin.site.register(Subject)
 admin.site.register(Provider)

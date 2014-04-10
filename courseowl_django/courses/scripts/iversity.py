d6b38512466136f12110d701fe012001fb55d2c4
Mike Staszel <crimsonredmk@gmail.com>
2014-04-10 16:04:57 -0500

    scraper is working - now just to add urls and test it

--- courseowl_django/courses/scripts/iversity.py
+++ courseowl_django/courses/scripts/iversity.py
@@ -1,13 +1,38 @@
 from courses.models import *
 import urllib2
 from bs4 import BeautifulSoup
-import re
 
 
 def run():
-    print("Adding courses from iVersity (this will take a minute)...")
+    print("Adding courses from iversity (this will take a minute)...")
     scrape()
 
 
 def scrape():
+    provider, created = Provider.objects.get_or_create(name='iversity')
     iversity_web = BeautifulSoup(urllib2.urlopen('https://iversity.org/courses').read())
+    course_divs = iversity_web.find_all('article', class_='courses-list-item')
+
+    for item in course_divs:
+        subject = item.find('div', class_='ribbon-content').text.strip('\n')
+        course_info = item.find('div', class_='course-body')
+        title = course_info.a.text
+        instructor_description_paragraphs = item.find_all('p')
+        instructor = instructor_description_paragraphs[0].text.strip('\n')
+        description = instructor_description_paragraphs[1].text.strip('\n')
+        url = item.a.attrs['href']  # TODO: Save the URL!
+
+        # print('===========================================')
+        # print('Adding course:')
+        # print('Subject: ' + subject)
+        # print('Title: ' + title)
+        # print('Instructor: ' + instructor)
+        # print('Description: ' + description)
+        # print('URL: ' + url)
+        # print('===========================================')
+
+        course, created = Course.objects.get_or_create(name=title, description=description, instructor=instructor,
+                                                       provider=provider)
+        subject, created = Subject.objects.get_or_create(name=subject)
+        course.subjects.add(subject)
+        course.save()

85440253de129eebbdbc66f245458d9d0dba7d62
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-04-10 21:07:39 -0500

    Tests should be less fickle now with downside that tests may take longer.

--- courseowl_django/frontend-tests/tests.js
+++ courseowl_django/frontend-tests/tests.js
@@ -1,6 +1,23 @@
-var siteUrl = 'http://courseowl.com/'; // not sure how to deal with production vs dev
+var siteUrl = 'http://localhost:8000/'; // not sure how to deal with production vs dev, TODO use Node env variables
+var timeoutTime = 3000;
+
+function navigationTest(test, url, assertionName) {
+  casper.waitForUrl(
+    url,
+    function then() {
+      test.pass(assertionName);
+    },
+    function timeout() {
+      test.fail(assertionName);
+    }, timeoutTime
+  );
+}
+
+var emailAddress = Date.now().toString() + '@t.co';
+var password = 'password';
 
 casper.test.begin('Homepage works', function suite(test) {
+  var navTest = navigationTest.bind(this, test);
   casper.start(siteUrl, function() {
      test.assertHttpStatus(200, 'Site is up and running');
      var signupButtonSelector = '#signup-button';
@@ -8,14 +25,10 @@ casper.test.begin('Homepage works', function suite(test) {
      this.click(signupButtonSelector);
   });
 
-  // Clicked signup button, should be on signup page
-  casper.then(function() {
-    test.assertEquals(
-      this.getCurrentUrl(),
-      (siteUrl + 'accounts/signup/'),
-      'Sign up page accessible from home page'
-    );
-  });
+  navTest(
+    siteUrl + 'accounts/signup/',
+    'Signup page accessible from home page'
+  );
 
   casper.thenOpen(siteUrl, function() {
     var signInSelector = '#signin-button';
@@ -23,14 +36,10 @@ casper.test.begin('Homepage works', function suite(test) {
     this.click(signInSelector);
   });
 
-  // Clicked sign in button
-  casper.then(function() {
-    test.assertEquals(
-      this.getCurrentUrl(),
-      (siteUrl + 'accounts/login/'),
-      'Login accessible from home page'
-    );
-  });
+  navTest(
+    siteUrl + 'accounts/login/',
+    'Login accessible from home page'
+  );
 
   casper.run(function() {
       test.done();
@@ -39,10 +48,9 @@ casper.test.begin('Homepage works', function suite(test) {
 
 casper.test.begin('Sign up process', function suite(test) {
   // TODO: find better way of testing new account creation by using temp accounts
-  var emailAddress = Date.now().toString() + '@t.co';
-  var password = 'password';
   var signupUrl = siteUrl + 'accounts/signup';
   var serverSideInvalid = 'div.alert';
+  var navTest = navigationTest.bind(this, test);
 
   casper.start(signupUrl, function() {
     test.assertExists('form', 'Sign up form exists');
@@ -93,11 +101,6 @@ casper.test.begin('Sign up process', function suite(test) {
       signupUrl,
       'Invalid email returns user to sign up page'
     );
-    // TODO fickle test
-    //test.assertExists(serverSideInvalid, 'Email warning message displayed');
-
-    // Clean up warning message
-    //this.click('button.close');
 
     // invalid passwords
     this.fill('form', {
@@ -114,9 +117,6 @@ casper.test.begin('Sign up process', function suite(test) {
       signupUrl,
       'Non matching passwords returns user to sign up page'
     );
-    // TODO fickle test
-    // test.assertExists(serverSideInvalid, 'Password warning message displayed');
-    //this.click('button.close');
 
     // Correct credentials to check next part of signup flow
     this.fill('form', {
@@ -126,80 +126,99 @@ casper.test.begin('Sign up process', function suite(test) {
     }, true);
   });
 
-  // We get to the personalization part of the sign up flow
+  navTest(
+    siteUrl + 'subject_preferences',
+    'Signup succesful and reached subject preferences'
+  );
+
   casper.then(function() {
-    // For some reason casper doesn't wait for the page to load
+    test.assertExists('.liked-subjects', 'Selected subject list exists');
+    test.assertDoesntExist('.liked-subjects .list-group-item', 'By default, no selected subjects');
+    test.assertExists('.other-subjects', 'All subjects list exists');
+    var numSubjects = this.evaluate(function() {
+      return __utils__.findAll('.other-subjects .list-group-item').length;
+    });
+    test.assert(numSubjects > 0, 'There exist subjects to choose from');
+    this.click('.other-subjects .list-group-item:first-child');
+
+    // The subject fades out upon removal, we have to wait for it
     this.wait(500, function() {
-      test.assertEquals(
-        this.getCurrentUrl(),
-        siteUrl + 'subject_preferences',
-        'The signup was successful and user is at personalization page'
-      );
-      test.assertExists('.liked-subjects', 'Selected subject list exists');
-      test.assertDoesntExist('.liked-subjects .list-group-item', 'By default, no selected subjects');
-      test.assertExists('.other-subjects', 'All subjects list exists');
-      var numSubjects = this.evaluate(function() {
+      test.assertExists('.liked-subjects .list-group-item', 'Subject is liked');
+      var numOtherSubjects = this.evaluate(function() {
         return __utils__.findAll('.other-subjects .list-group-item').length;
       });
-      test.assert(numSubjects > 0, 'There exist subjects to choose from');
-      this.click('.other-subjects .list-group-item:first-child');
+      test.assert(numOtherSubjects < numSubjects, 'Subject removed from unliked subjects');
 
-      // The subject fades out upon removal, we have to wait for it
-      this.wait(500, function() {
-        test.assertExists('.liked-subjects .list-group-item', 'Subject is liked');
-        var numOtherSubjects = this.evaluate(function() {
-          return __utils__.findAll('.other-subjects .list-group-item').length;
-        });
-        test.assert(numOtherSubjects < numSubjects, 'Subject removed from unliked subjects');
-
-        this.click('#submit-subjects');
-      });
+      this.click('#submit-subjects');
     });
   });
 
+  navTest(
+    siteUrl + 'course_preferences',
+    'Advanced from subject preferences to course preferences'
+  );
+
   casper.then(function() {
-    // Wait for course preferences page to load
+    test.assertExists('.enrolled-courses', 'Enrolled course list exists');
+    test.assertDoesntExist('.enrolled-courses .list-group-item', 'No course enrolled in by default');
+    test.assertExists('.other-courses', 'Course list available');
+    var origNumCourses = this.evaluate(function() {
+      return __utils__.findAll('.other-courses .list-group-item').length;
+    });
+    test.assert(origNumCourses > 0, 'There are courses to choose from');
+    this.click('.other-courses .list-group-item:first-child');
+
+    // wait for fade out
     this.wait(500, function() {
-      test.assertEquals(
-        this.getCurrentUrl(),
-        siteUrl + 'course_preferences',
-        'We moved from subject preferences to course preferences'
-      );
-
-      test.assertExists('.enrolled-courses', 'Enrolled course list exists');
-      test.assertDoesntExist('.enrolled-courses .list-group-item', 'No course enrolled in by default');
-      test.assertExists('.other-courses', 'Course list available');
-      var origNumCourses = this.evaluate(function() {
-        return __utils__.findAll('.other-courses .list-group-item').length;
+      test.assertExists('.enrolled-courses .list-group-item', 'A course is enrolled in');
+      var currNumCourses = this.evaluate(function() {
+        return __utils__.findAll('.other-course .list-group-item').length;
       });
-      test.assert(origNumCourses > 0, 'There are courses to choose from');
-      this.click('.other-courses .list-group-item:first-child');
+      test.assert(currNumCourses < origNumCourses, 'The course was removed from all courses list');
 
-      // wait for fade out
-      this.wait(500, function() {
-        test.assertExists('.enrolled-courses .list-group-item', 'A course is enrolled in');
-        var currNumCourses = this.evaluate(function() {
-          return __utils__.findAll('.other-course .list-group-item').length;
-        });
-        test.assert(currNumCourses < origNumCourses, 'The course was removed from all courses list');
-
-        this.click('#submit-courses');
-      })
-
+      this.click('#submit-courses');
     });
   });
+// TODO check that submitted are the same as backend's courses/subjects
+
+  navTest(
+    siteUrl + 'accounts/profile',
+    'Signup flow completed'
+  );
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin('Profile page functional', function suite(test) {
+  var navTest = navigationTest.bind(this, test);
+  casper.start(siteUrl, function() {
+    test.assertExists('#signout-button', 'Authenticated user has option to signout on homepage');
+    test.assertExists('#profile-button', 'Authenticated user can navigate to profile from home page');
+    this.click('#profile-button');
+  });
+
+  navTest(
+    siteUrl + 'accounts/profile',
+    'Authenticated user reached profile page from home page'
+  );
 
   casper.then(function() {
-    this.wait(500, function() {
-      test.assertEquals(
-        this.getCurrentUrl(),
-        siteUrl + 'accounts/profile',
-        'Sign up flow completed'
-      );
-    });
+    // Check that user is enrolled in one course
+    test.assertExists('.enrolled-courses', 'User can see a table of enrolled courses');
+    test.assertElementCount('.enrolled-courses tr', 1, 'This user is enrolled in the one course from their signup flow');
+
+    // Check that user can choose from recommended courses
+    test.assertExists('.recommended-courses', 'Recommended course table exists');
+    test.assertExists('.recommended-courses tr', 'There exist recommended courses');
   });
 
   casper.run(function() {
     test.done();
   });
+});
+
+casper.test.begin('Deleting account', function suite(test) {
+
 });

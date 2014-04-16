712247752786788090058c10aba2506c1cf12e12
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-04-15 21:18:49 -0500

    Moved js loading to base.html; moved duplicated modal code to js helper and modal template html file; fixed css on search results page

--- courseowl_django/website/static/js/personalize_helper.js
+++ courseowl_django/website/static/js/personalize_helper.js
@@ -1,4 +1,4 @@
-// Sets click listener to list items to move/animate movement between lists 
+// Sets click listener to list items to move/animate movement between lists
 function setSwitchActions(firstList, secondList) {
   var setMoveAction = function(currentList, newList) {
     $(currentList).on('click', '.list-group-item', function(event) {
@@ -8,7 +8,7 @@ function setSwitchActions(firstList, secondList) {
         $movingItem.fadeIn();
       });
     });
-  } 
+  }
 
   setMoveAction(firstList, secondList);
   setMoveAction(secondList, firstList);
@@ -25,5 +25,28 @@ function bundleSelectionsOnSubmit(values) {
     $('form').append(
       $('<input type="hidden" value="' + JSON.stringify(ids) + '" name="' + values.inputName + '">')
     );
+  });
+}
+
+/*
+ * Displays info modal when a user clicks on the info button for that course
+ */
+function setInfoModalListener() {
+  $('.info-button').on('click', function(event) {
+    var courseID = $(this).closest('tr').data('id');
+    console.log("Course ID: ", courseID);
+    $.post(
+      '/api/course_info',
+      { course_id : courseID }
+    ).done(function(res){
+      console.dir(res);
+      $('#course_name').text(res.info.name);
+      $('#provider').text(res.info.provider);
+      $('#subject').text(res.info.subjects);
+      $('#course_ins').text(res.info.instructor);
+      $('#course_desc').text(res.info.description);
+      $('#url').text(res.info.url);
+      $('#url').attr('href', res.info.url);
+    });
   });
 }
